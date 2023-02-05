5500c374990642902783decf697edb1f77c89145
Kittipan Pakmoei <65502586+KittipanTa@users.noreply.github.com>
2023-02-05 15:43:47 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace software_to_print_image
+namespace software_calculate_distance_from_gas
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+    
+        class Vehicle
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            private double gasLiter;
+            private const int wheelCircumference = 95;
+            private const int roundsPerLiter = 13000;
+
+            public Vehicle(double gasLiter)
+            {
+                this.gasLiter = gasLiter;
+            }
+
+            public double CalculateDistance()
+            {
+                return (gasLiter * roundsPerLiter * wheelCircumference) / 100000;
+            }
         }
-    }
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+            while (true)
+            {
+                Console.Write("Enter the amount of gas in liters:");
+                string gasLiter = Console.ReadLine();
+                double number;
+                if (double.TryParse(gasLiter, out number) && number > 2)
+                {
+                    Vehicle vehicle = new Vehicle(number);
+                    double distance = vehicle.CalculateDistance();
+                    Console.WriteLine("Distance: " + distance + " KM");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a integer number ");
+                }
+                Console.WriteLine("Again y/n");
+                string ck = Console.ReadLine().ToString();
+                if (ck == "n")
+                {
+                    break;
+                }
+                Console.Clear();
+            }
+            
+            
+            }
+        }
+    
 }
